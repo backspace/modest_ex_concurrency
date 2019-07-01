e326b53bbe5ff679fa945a7d689cc6ed7f0c95fc
Buck Doyle <b@chromatin.ca>
2019-07-01 16:39:43 -0500

    Add test to exercise ModestEx

--- test/modest_ex_concurrency_web/controllers/page_controller_test.exs
+++ test/modest_ex_concurrency_web/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule ModestExConcurrencyWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
+
+  test "serialises a string" do
+    assert ModestEx.serialize("<div>Hello<span>World", :body_children) ==
+             "<div>Hello<span>World</span></div>"
+  end
 end
